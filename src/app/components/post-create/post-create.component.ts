26a38a51f0986742d1fb042468e4e92d4a17b1cb
Binod Shrestha <shresthabinod.softdev@gmail.com>
2021-01-27 23:08:01 -0500

    code refactored

--- src/app/components/post-create/post-create.component.ts
+++ src/app/components/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 import {Post} from '../models/post.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -18,8 +19,12 @@ export class PostCreateComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddPost(): void {
-    const post: Post = {title: this.enteredTitle, content: this.enteredContent};
+  onAddPost(form: NgForm): void {
+    if (form.invalid){
+      return;
+    }
+
+    const post: Post = {title: form.value.title, content: form.value.content};
     this.postCreated.emit(post);
   }
 
